60edea57cf24b8a7b64e7202faee4e10ce20f747
aliceconcordel <alice.concordel@gmail.com>
2017-07-24 08:36:51 +0200

    Revert "[Dronecourse] mavlink: added variance to target_position_ned"
    This reverts commit d7c408f98af76770ad85c7f4c79fdf0a3aad0635.

--- include/mavlink/v1.0/dronecourse/mavlink_msg_target_position_ned.h
+++ include/mavlink/v1.0/dronecourse/mavlink_msg_target_position_ned.h
@@ -8,19 +8,16 @@ typedef struct __mavlink_target_position_ned_t {
  float x; /*< X Position in NED frame in meters*/
  float y; /*< Y Position in NED frame in meters*/
  float z; /*< Z Position in NED frame in meters*/
- float var_x; /*< X Position variance in NED frame in meters^2*/
- float var_y; /*< Y Position variance in NED frame in meters^2*/
- float var_z; /*< Z Position variance in NED frame in meters^2*/
  uint8_t target_num; /*< ID of the target object.*/
 }) mavlink_target_position_ned_t;
 
-#define MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN 25
-#define MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN 25
-#define MAVLINK_MSG_ID_150_LEN 25
-#define MAVLINK_MSG_ID_150_MIN_LEN 25
+#define MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN 13
+#define MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN 13
+#define MAVLINK_MSG_ID_150_LEN 13
+#define MAVLINK_MSG_ID_150_MIN_LEN 13
 
-#define MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC 206
-#define MAVLINK_MSG_ID_150_CRC 206
+#define MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC 136
+#define MAVLINK_MSG_ID_150_CRC 136
 
 
 
@@ -28,27 +25,21 @@ typedef struct __mavlink_target_position_ned_t {
 #define MAVLINK_MESSAGE_INFO_TARGET_POSITION_NED { \
     150, \
     "TARGET_POSITION_NED", \
-    7, \
+    4, \
     {  { "x", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_target_position_ned_t, x) }, \
          { "y", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_target_position_ned_t, y) }, \
          { "z", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_target_position_ned_t, z) }, \
-         { "var_x", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_target_position_ned_t, var_x) }, \
-         { "var_y", NULL, MAVLINK_TYPE_FLOAT, 0, 16, offsetof(mavlink_target_position_ned_t, var_y) }, \
-         { "var_z", NULL, MAVLINK_TYPE_FLOAT, 0, 20, offsetof(mavlink_target_position_ned_t, var_z) }, \
-         { "target_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 24, offsetof(mavlink_target_position_ned_t, target_num) }, \
+         { "target_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 12, offsetof(mavlink_target_position_ned_t, target_num) }, \
          } \
 }
 #else
 #define MAVLINK_MESSAGE_INFO_TARGET_POSITION_NED { \
     "TARGET_POSITION_NED", \
-    7, \
+    4, \
     {  { "x", NULL, MAVLINK_TYPE_FLOAT, 0, 0, offsetof(mavlink_target_position_ned_t, x) }, \
          { "y", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_target_position_ned_t, y) }, \
          { "z", NULL, MAVLINK_TYPE_FLOAT, 0, 8, offsetof(mavlink_target_position_ned_t, z) }, \
-         { "var_x", NULL, MAVLINK_TYPE_FLOAT, 0, 12, offsetof(mavlink_target_position_ned_t, var_x) }, \
-         { "var_y", NULL, MAVLINK_TYPE_FLOAT, 0, 16, offsetof(mavlink_target_position_ned_t, var_y) }, \
-         { "var_z", NULL, MAVLINK_TYPE_FLOAT, 0, 20, offsetof(mavlink_target_position_ned_t, var_z) }, \
-         { "target_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 24, offsetof(mavlink_target_position_ned_t, target_num) }, \
+         { "target_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 12, offsetof(mavlink_target_position_ned_t, target_num) }, \
          } \
 }
 #endif
@@ -62,24 +53,18 @@ typedef struct __mavlink_target_position_ned_t {
  * @param x X Position in NED frame in meters
  * @param y Y Position in NED frame in meters
  * @param z Z Position in NED frame in meters
- * @param var_x X Position variance in NED frame in meters^2
- * @param var_y Y Position variance in NED frame in meters^2
- * @param var_z Z Position variance in NED frame in meters^2
  * @param target_num ID of the target object.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_target_position_ned_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               float x, float y, float z, float var_x, float var_y, float var_z, uint8_t target_num)
+                               float x, float y, float z, uint8_t target_num)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN];
     _mav_put_float(buf, 0, x);
     _mav_put_float(buf, 4, y);
     _mav_put_float(buf, 8, z);
-    _mav_put_float(buf, 12, var_x);
-    _mav_put_float(buf, 16, var_y);
-    _mav_put_float(buf, 20, var_z);
-    _mav_put_uint8_t(buf, 24, target_num);
+    _mav_put_uint8_t(buf, 12, target_num);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN);
 #else
@@ -87,9 +72,6 @@ static inline uint16_t mavlink_msg_target_position_ned_pack(uint8_t system_id, u
     packet.x = x;
     packet.y = y;
     packet.z = z;
-    packet.var_x = var_x;
-    packet.var_y = var_y;
-    packet.var_z = var_z;
     packet.target_num = target_num;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN);
@@ -108,25 +90,19 @@ static inline uint16_t mavlink_msg_target_position_ned_pack(uint8_t system_id, u
  * @param x X Position in NED frame in meters
  * @param y Y Position in NED frame in meters
  * @param z Z Position in NED frame in meters
- * @param var_x X Position variance in NED frame in meters^2
- * @param var_y Y Position variance in NED frame in meters^2
- * @param var_z Z Position variance in NED frame in meters^2
  * @param target_num ID of the target object.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_target_position_ned_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   float x,float y,float z,float var_x,float var_y,float var_z,uint8_t target_num)
+                                   float x,float y,float z,uint8_t target_num)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN];
     _mav_put_float(buf, 0, x);
     _mav_put_float(buf, 4, y);
     _mav_put_float(buf, 8, z);
-    _mav_put_float(buf, 12, var_x);
-    _mav_put_float(buf, 16, var_y);
-    _mav_put_float(buf, 20, var_z);
-    _mav_put_uint8_t(buf, 24, target_num);
+    _mav_put_uint8_t(buf, 12, target_num);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN);
 #else
@@ -134,9 +110,6 @@ static inline uint16_t mavlink_msg_target_position_ned_pack_chan(uint8_t system_
     packet.x = x;
     packet.y = y;
     packet.z = z;
-    packet.var_x = var_x;
-    packet.var_y = var_y;
-    packet.var_z = var_z;
     packet.target_num = target_num;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN);
@@ -156,7 +129,7 @@ static inline uint16_t mavlink_msg_target_position_ned_pack_chan(uint8_t system_
  */
 static inline uint16_t mavlink_msg_target_position_ned_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_target_position_ned_t* target_position_ned)
 {
-    return mavlink_msg_target_position_ned_pack(system_id, component_id, msg, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->var_x, target_position_ned->var_y, target_position_ned->var_z, target_position_ned->target_num);
+    return mavlink_msg_target_position_ned_pack(system_id, component_id, msg, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->target_num);
 }
 
 /**
@@ -170,7 +143,7 @@ static inline uint16_t mavlink_msg_target_position_ned_encode(uint8_t system_id,
  */
 static inline uint16_t mavlink_msg_target_position_ned_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_target_position_ned_t* target_position_ned)
 {
-    return mavlink_msg_target_position_ned_pack_chan(system_id, component_id, chan, msg, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->var_x, target_position_ned->var_y, target_position_ned->var_z, target_position_ned->target_num);
+    return mavlink_msg_target_position_ned_pack_chan(system_id, component_id, chan, msg, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->target_num);
 }
 
 /**
@@ -180,24 +153,18 @@ static inline uint16_t mavlink_msg_target_position_ned_encode_chan(uint8_t syste
  * @param x X Position in NED frame in meters
  * @param y Y Position in NED frame in meters
  * @param z Z Position in NED frame in meters
- * @param var_x X Position variance in NED frame in meters^2
- * @param var_y Y Position variance in NED frame in meters^2
- * @param var_z Z Position variance in NED frame in meters^2
  * @param target_num ID of the target object.
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_target_position_ned_send(mavlink_channel_t chan, float x, float y, float z, float var_x, float var_y, float var_z, uint8_t target_num)
+static inline void mavlink_msg_target_position_ned_send(mavlink_channel_t chan, float x, float y, float z, uint8_t target_num)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN];
     _mav_put_float(buf, 0, x);
     _mav_put_float(buf, 4, y);
     _mav_put_float(buf, 8, z);
-    _mav_put_float(buf, 12, var_x);
-    _mav_put_float(buf, 16, var_y);
-    _mav_put_float(buf, 20, var_z);
-    _mav_put_uint8_t(buf, 24, target_num);
+    _mav_put_uint8_t(buf, 12, target_num);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TARGET_POSITION_NED, buf, MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC);
 #else
@@ -205,9 +172,6 @@ static inline void mavlink_msg_target_position_ned_send(mavlink_channel_t chan, 
     packet.x = x;
     packet.y = y;
     packet.z = z;
-    packet.var_x = var_x;
-    packet.var_y = var_y;
-    packet.var_z = var_z;
     packet.target_num = target_num;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TARGET_POSITION_NED, (const char *)&packet, MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC);
@@ -222,7 +186,7 @@ static inline void mavlink_msg_target_position_ned_send(mavlink_channel_t chan, 
 static inline void mavlink_msg_target_position_ned_send_struct(mavlink_channel_t chan, const mavlink_target_position_ned_t* target_position_ned)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_target_position_ned_send(chan, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->var_x, target_position_ned->var_y, target_position_ned->var_z, target_position_ned->target_num);
+    mavlink_msg_target_position_ned_send(chan, target_position_ned->x, target_position_ned->y, target_position_ned->z, target_position_ned->target_num);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TARGET_POSITION_NED, (const char *)target_position_ned, MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC);
 #endif
@@ -236,17 +200,14 @@ static inline void mavlink_msg_target_position_ned_send_struct(mavlink_channel_t
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_target_position_ned_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  float x, float y, float z, float var_x, float var_y, float var_z, uint8_t target_num)
+static inline void mavlink_msg_target_position_ned_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  float x, float y, float z, uint8_t target_num)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
     _mav_put_float(buf, 0, x);
     _mav_put_float(buf, 4, y);
     _mav_put_float(buf, 8, z);
-    _mav_put_float(buf, 12, var_x);
-    _mav_put_float(buf, 16, var_y);
-    _mav_put_float(buf, 20, var_z);
-    _mav_put_uint8_t(buf, 24, target_num);
+    _mav_put_uint8_t(buf, 12, target_num);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TARGET_POSITION_NED, buf, MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC);
 #else
@@ -254,9 +215,6 @@ static inline void mavlink_msg_target_position_ned_send_buf(mavlink_message_t *m
     packet->x = x;
     packet->y = y;
     packet->z = z;
-    packet->var_x = var_x;
-    packet->var_y = var_y;
-    packet->var_z = var_z;
     packet->target_num = target_num;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TARGET_POSITION_NED, (const char *)packet, MAVLINK_MSG_ID_TARGET_POSITION_NED_MIN_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN, MAVLINK_MSG_ID_TARGET_POSITION_NED_CRC);
@@ -300,43 +258,13 @@ static inline float mavlink_msg_target_position_ned_get_z(const mavlink_message_
 }
 
 /**
- * @brief Get field var_x from target_position_ned message
- *
- * @return X Position variance in NED frame in meters^2
- */
-static inline float mavlink_msg_target_position_ned_get_var_x(const mavlink_message_t* msg)
-{
-    return _MAV_RETURN_float(msg,  12);
-}
-
-/**
- * @brief Get field var_y from target_position_ned message
- *
- * @return Y Position variance in NED frame in meters^2
- */
-static inline float mavlink_msg_target_position_ned_get_var_y(const mavlink_message_t* msg)
-{
-    return _MAV_RETURN_float(msg,  16);
-}
-
-/**
- * @brief Get field var_z from target_position_ned message
- *
- * @return Z Position variance in NED frame in meters^2
- */
-static inline float mavlink_msg_target_position_ned_get_var_z(const mavlink_message_t* msg)
-{
-    return _MAV_RETURN_float(msg,  20);
-}
-
-/**
  * @brief Get field target_num from target_position_ned message
  *
  * @return ID of the target object.
  */
 static inline uint8_t mavlink_msg_target_position_ned_get_target_num(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint8_t(msg,  24);
+    return _MAV_RETURN_uint8_t(msg,  12);
 }
 
 /**
@@ -351,9 +279,6 @@ static inline void mavlink_msg_target_position_ned_decode(const mavlink_message_
     target_position_ned->x = mavlink_msg_target_position_ned_get_x(msg);
     target_position_ned->y = mavlink_msg_target_position_ned_get_y(msg);
     target_position_ned->z = mavlink_msg_target_position_ned_get_z(msg);
-    target_position_ned->var_x = mavlink_msg_target_position_ned_get_var_x(msg);
-    target_position_ned->var_y = mavlink_msg_target_position_ned_get_var_y(msg);
-    target_position_ned->var_z = mavlink_msg_target_position_ned_get_var_z(msg);
     target_position_ned->target_num = mavlink_msg_target_position_ned_get_target_num(msg);
 #else
         uint8_t len = msg->len < MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN? msg->len : MAVLINK_MSG_ID_TARGET_POSITION_NED_LEN;
